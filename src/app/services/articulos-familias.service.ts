a6ae6c63b87600843b5078e9be4cf71e5d662293
gaticafederico <gaticafederico4@gmail.com>
2022-08-28 15:06:24 -0300

    Initial commit

---
+++ src/app/services/articulos-familias.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { ArticuloFamilia } from '../models/articulo-familia';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticulosFamiliasService {
+  resourceUrl: string;
+  constructor(private httpClient: HttpClient) {
+    this.resourceUrl =
+      environment.ConexionWebApiProxy + 'articulosfamilias' + '/';
+    //this.resourceUrl = 'https://localhost:44399/api/articulosfamilias' + '/';
+
+    //this.resourceUrl = 'https://node-wk1acm--5000.local.webcontainer.io/articulosfamilias' + '/';
+  }
+
+  get() {
+    return this.httpClient.get(this.resourceUrl);
+  }
+  // mismo metodo tipado
+  //  get():Observable<ArticuloFamilia[]> {
+  //   return this.httpClient.get<ArticuloFamilia[]>(this.resourceUrl);
+  // }
+}
